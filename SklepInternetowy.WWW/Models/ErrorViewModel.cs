63bdcf4d694f99c53caac293216620f8b60dbeb3
PatrycjaMicle <patrycja.starmach1993@gmail.com>
2024-01-02 14:03:24 +0100

    Create initial Web project

---
+++ SklepInternetowy.WWW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SklepInternetowy.WWW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
