ad6a06f442b612bbb50ae3f53663cbb82afa47aa
PatrycjaMicle <patrycja.starmach1993@gmail.com>
2024-01-03 02:40:43 +0100

    * remove navigation for RolaUzytkownika since there are again problems with deserialisation * Add new Migration

---
+++ RestApiZamowienia/Migrations/20240103013457_remove_RolaUzytkownikaNavigation1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestApiZamowienia.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_RolaUzytkownikaNavigation1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Uzytokwnik_RolaUzytkownika",
+                table: "Uzytkownik");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Uzytkownik_RolaUzytkownikaId",
+                table: "Uzytkownik");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Uzytkownik_RolaUzytkownikaId",
+                table: "Uzytkownik",
+                column: "RolaUzytkownikaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Uzytokwnik_RolaUzytkownika",
+                table: "Uzytkownik",
+                column: "RolaUzytkownikaId",
+                principalTable: "RolaUzytkownika",
+                principalColumn: "Id");
+        }
+    }
+}
--- RestApiZamowienia/Models/RolaUzytkownika.cs
+++ RestApiZamowienia/Models/RolaUzytkownika.cs
@@ -6,7 +6,4 @@ public class RolaUzytkownika
 {
     public int Id { get; set; }
     public string Name { get; set; }
-
-    [InverseProperty("RolaUzytkownikaIdNavigation")]
-    public virtual ICollection<Uzytkownik> Uzytkowniks { get; set; } = new List<Uzytkownik>();
 }
