837ce20831ed11c8b315a91080835de5f8c7a63c
PatrycjaMicle <patrycja.starmach1993@gmail.com>
2024-01-04 18:54:36 +0100

    * push unversioned files

---
+++ RestApiZamowienia/Migrations/20240104174806_create_Promocja_and_Kod_ManyToMany1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestApiZamowienia.Migrations
+{
+    /// <inheritdoc />
+    public partial class create_Promocja_and_Kod_ManyToMany1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
